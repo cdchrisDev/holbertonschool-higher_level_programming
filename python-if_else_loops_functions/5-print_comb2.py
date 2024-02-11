1b7b028004716535f3d0afe574d4baf7baaa5f47
cdchrisDev <juan.feli.lopez@gmail.com>
2024-02-11 08:44:34 -0800

    remade

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i == 99:
-        print("{}".format(i))
-    else:
-        print("{}, ".format(i), end="")
+for i in range(0, 99):
+    print("{}, ".format(i), end="")
+print(i+1)
