bde8abcb22a68ac6d8de1255eff014aac67cb5b1
cdchrisDev <juan.feli.lopez@gmail.com>
2024-02-11 09:16:55 -0800

    done

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 i = 1
 for x in range(9):
     for y in range(i, 10):
@@ -6,4 +7,4 @@ for x in range(9):
             print('{:d}{:d}, '.format(x, y), end="")
         else:
             print('{:d}{:d}'.format(x, y))
-i += 1
+    i += 1
