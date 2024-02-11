ffa6bc86e11789312f9890ba0454a1c20374c7e1
cdchrisDev <juan.feli.lopez@gmail.com>
2024-02-11 11:45:20 -0800

    multi 5 and 3 fixed

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 99):
+        if i % 3 == 0 and i % 5 == 0:
+            i = "FizzBuzz"
+        elif i % 5 == 0:
+            i = "Buzz"
+        elif i % 3 == 0:
+            i == "Fizz"
+
+        print("{} ".format(i), end="")
+    print("Buzz")
