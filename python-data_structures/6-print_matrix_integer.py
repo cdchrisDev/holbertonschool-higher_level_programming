872e9affe421565a35e68a3d484b4e45c7f962cd
cdchrisDev <juan.feli.lopez@gmail.com>
2024-02-16 02:21:09 -0800

    odne

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print(" ".join([f"{item}" for item in row]))
+    if len(matrix) == 0:
+       print()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j < len(matrix[i]) - 1:
+                print(" ", end="")
+        print()
