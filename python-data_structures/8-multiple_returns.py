e8f1e7a04aca183ab689e28188d214bdf208bef1
cdchrisDev <juan.feli.lopez@gmail.com>
2024-02-16 16:46:50 -0800

    mispelling fixed

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def multiple_returns(sentences):
-    if not sentences:
-        sentences[0] = None
+def multiple_returns(sentence):
+    if not sentence:
+        sentence[0] = None
 
-    return len(sentences), sentences[0]
+    return len(sentence), sentences[0]
