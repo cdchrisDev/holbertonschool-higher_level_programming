abf95e509e46640cc5021780e143b2e4a61c09b6
cdchrisDev <juan.feli.lopez@gmail.com>
2024-02-18 13:11:29 -0800

    done

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     new_ls = list(sorted(a_dictionary))
-    for i in range(len(new)):
-        print("{:s}: {}".format(new_ls[i], a_dictionary[new[i]]))
+    for i in range(len(new_ls)):
+        print("{:s}: {}".format(new_ls[i], a_dictionary[new_ls[i]]))
