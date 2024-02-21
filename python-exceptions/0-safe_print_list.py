b9890b804f0e588502d0c3c7559259f7e757f47a
cdchrisDev <juan.feli.lopez@gmail.com>
2024-02-20 16:00:41 -0800

    indentation

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     nb = 0
-    try:
-        for y in range(x):
-            print(my_list[i], end="")
+    for i in range(0, x):
+        try:
+            print(f'{my_list[i]}', end="")
             nb += 1
-    except:
-        pass
-    finally:
-        print("")
+        except (IndexError):
+            pass
+    print("")
     return nb
