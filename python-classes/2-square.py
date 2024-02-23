504676eed060f455e5e5a1fe0d4871304d8f2aa0
cdchrisDev <juan.feli.lopez@gmail.com>
2024-02-23 10:44:18 +0000

    1st try

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A class 'Square' With exceptions"""
+
+
+class Square:
+    """Define a square with exceptions"""
+
+    def __init__(self, size=0):
+        """Start"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = size
