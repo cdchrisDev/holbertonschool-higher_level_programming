f6908ee2c7152597ca9a279f67b17a998d5f975d
cdchrisDev <juan.feli.lopez@gmail.com>
2024-02-23 10:38:02 +0000

    1st try

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """A class defined private attr size using ' __ '(underscore)"""
+
+    def __init__(self, size):
+        self.__size = size
