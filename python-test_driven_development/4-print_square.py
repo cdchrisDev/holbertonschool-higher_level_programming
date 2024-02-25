6b6665764dbc3710ff0ae619b8c445a3375fa2fb
cdchrisDev <juan.feli.lopez@gmail.com>
2024-02-25 16:51:17 +0000

    2nd try

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,7 +5,7 @@
 def print_square(size):
     """print a square of size with #"""
     if not isinstance(size, int):
-        raise TypeError(size, int)
+        raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
 
