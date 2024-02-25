48d2ac85f26fc966cdc07dc9e83c7318dfbb1bf1
cdchrisDev <juan.feli.lopez@gmail.com>
2024-02-25 18:02:27 +0000

    1st try

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""5-text_indentation module"""
+
+
+def text_indentation(text):
+    """indented text"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    output = ""
+    for char in text:
+        output += char
+        if char in ".:?":
+            output += "\n\n"
+
+    lines = output.splitlines()
+    indented_text = "\n".join(line.strip() for line in lines)
+    print(indented_text, end="")
