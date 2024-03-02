5b4819b5e3a6ed32f675db44f729fb3921d3ea0d
cdchrisDev <juan.feli.lopez@gmail.com>
2024-03-02 23:53:25 +0000

    valid attr 1st

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -38,6 +38,10 @@ class Rectangle(Base):
         @width.setter
         def width(self, value):
             """ set the width for rectangle """
+            if type(value) != int:
+                raise TypeError("width must be an integer")
+            if value <= 0:
+                raise ValueError("width must be > 0")
             self.__width = value
 
         @property
@@ -48,6 +52,10 @@ class Rectangle(Base):
         @height.setter
         def height(self, value):
             """ Set the height for rectangle """
+            if type(value) != int:
+                raise TypeError("height must be an integer")
+            if value <= 0:
+                raise ValueError("height must be > 0")
             self.__height = value
 
         @property
@@ -58,6 +66,10 @@ class Rectangle(Base):
         @x.setter
         def x(self, value):
             """ Set the x value """
+            if type(value) != int:
+                raise TypeError("x must be an integer")
+            if value <= 0:
+                raise ValueError("x must be > 0")
             self.__x = value
 
         @property
@@ -68,4 +80,8 @@ class Rectangle(Base):
         @y.setter
         def y(self, value):
             """ set the y value """
+            if type(value) != int:
+                raise TypeError("y must be an integer")
+            if value <= 0:
+                raise ValueError("y must be > 0")
             self.__y = value
