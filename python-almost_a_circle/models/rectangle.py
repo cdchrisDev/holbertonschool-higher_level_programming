65267773875d050b42428d1b2d97637123d1a0a7
cdchrisDev <juan.feli.lopez@gmail.com>
2024-03-02 23:29:04 +0000

    4th try

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -24,9 +24,8 @@ class Rectangle(Base):
             y (int): default 0
             id ([type]): Default None
         """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
