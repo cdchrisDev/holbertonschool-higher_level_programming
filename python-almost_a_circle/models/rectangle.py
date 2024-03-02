9e56cec264af33d64377508b8b23e2ecfe7c8448
cdchrisDev <juan.feli.lopez@gmail.com>
2024-03-02 23:45:58 +0000

    6th try

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -32,32 +32,40 @@ class Rectangle(Base):
 
         @property
         def width(self):
+            """ Width getter for rectangle """
             return self.__width
 
         @width.setter
         def width(self, value):
+            """ set the width for rectangle """
             self.__width = value
 
         @property
         def height(self):
+            """ get the height for rectangle """
             return self.__height
 
         @height.setter
         def height(self, value):
+            """ Set the height for rectangle """
             self.__height = value
 
         @property
         def x(self):
+            """ Get the x value """
             return self.__x
 
         @x.setter
         def x(self, value):
+            """ Set the x value """
             self.__x = value
 
         @property
         def y(self):
+            """ get the y value """
             return self.__y
 
         @y.setter
         def y(self, value):
+            """ set the y value """
             self.__y = value
