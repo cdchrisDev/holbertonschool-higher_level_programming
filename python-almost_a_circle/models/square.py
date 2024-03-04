f40adc9810cef34fc367cd8d3ec4f54fee79a627
cdchrisDev <juan.feli.lopez@gmail.com>
2024-03-04 20:27:36 +0000

    square 1st try

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''A class inherited from models rectangle'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''This is the definition of a square
+
+       args:
+       x(int): x value
+       y(int)
+       id: Bool:
+    '''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Superclass args width and height must replace size'''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''overloding the method'''
+
+        return "[square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.height)
+
