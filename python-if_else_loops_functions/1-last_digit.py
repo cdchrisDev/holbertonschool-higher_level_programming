07ba641bdad603a1856bf48e62f40a83ae4ff16e
cdchrisDev <juan.feli.lopez@gmail.com>
2024-05-19 09:53:56 -0500

    upload

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,6 @@ dig = abs(number) % 10
 print(f"Last digit of {number} is ", end="")
 if number < 0:
     dig = -dig
-    
 if dig < 6 and dig != 0:
     print(f"{dig} and is less than 6 and not zero")
 elif dig == 0:
---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/python3
+for i in range(0, 10):
+    for j in range(i, 10):
+        if j == i:
+            continue
+        elif i == 8:
+            print(f"{i}{j}")
+        else:
+            print(f"{i}{j}, ", end="")
