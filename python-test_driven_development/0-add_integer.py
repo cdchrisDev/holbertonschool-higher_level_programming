2e851dc2314e5dcf8ead0ba8ebe9f3552206e291
cdchrisDev <juan.feli.lopez@gmail.com>
2024-05-21 17:55:21 -0500

    second try

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""The 0-add_integer module"""
+
+
 def add_integer(a, b=98):
+    """Function adds two integers
+    Args:
+        a ((int), (float)): first arg
+        b ((int), (float)): second arg. Default to 98
+        
+        return: result op
+    """
     if isinstance(a, (float, int)) is False:
         raise TypeError("a must be an integer".format(a))
     if isinstance(b, (float, int)) is False:
