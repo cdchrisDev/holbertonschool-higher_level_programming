8048e95495d45f9eede44085d7ea4017a0756ca1
cdchrisDev <juan.feli.lopez@gmail.com>
2024-05-26 16:58:12 -0500

    pir

---
+++ python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is the Square module"""
+
+
+class Square:
+    """Define a square"""
+    
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is the square module"""
+
+
+class Square:
+    """define a square"""
+
+    def __init__(self, size):
+        """init square"""
+        
+        self.__size = size
