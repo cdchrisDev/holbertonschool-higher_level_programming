0747acc1fa14186ba31f31e88475c7818213a4d9
cdchrisDev <juan.feli.lopez@gmail.com>
2024-06-13 16:35:01 -0500

    4th try

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,14 +6,23 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle"""
-
+    """A class that define squares
+    based on the Rectangle Obj, based
+    on BaseGeometry Obj
+    """
+    
     def __init__(self, size):
-        """constructs with size attribute"""
+        """Constructor
+        
+        size (int): the size of each side of
+        geometrical Obj
+        """
+        
         self.__size = size
         super().__init__(self.__size, self.__size)
         self.integer_validator("size", size)
-
+        
     def area(self):
-        """area method"""
+        """A func that define the area of a Obj"""
+        
         return self.__size ** 2
