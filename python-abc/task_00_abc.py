73dae877b84cbe36fc911c1502eba63a1e5e820d
cdchrisDev <juan.feli.lopez@gmail.com>
2024-06-15 18:25:02 -0500

    1st try [Not pycodestyle]

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This is the task__00_abc
+Module
+"""
+from abc import ABCMeta, abstractmethod
+
+
+class Animal(metaclass=ABCMeta):
+    """An abstract class that define
+    variations for animal from
+    ABCMeta
+    """
+    @abstractmethod
+    def sound(self):
+        """A general sound method"""
+        pass
+    
+class Dog(Animal):
+    """A class that define a Dog base
+    on Animal class
+    """
+    
+    def sound(self):
+        return "Bark"
+    
+class Cat(Animal):
+    """A class that define a cat base
+    on Animal class
+    """
+    
+    def sound(self):
+        return "Meow"
---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""This is the task_01_duck_typing
+Module
+"""
+from abc import ABCMeta, abstractmethod
+import math
+
+
+class Shape(metaclass=ABCMeta):
+    """An abstract class that define
+    de variations of a Shape class
+    """
+    
+    @abstractmethod
+    def area(self):
+        """An abstract method area"""
+        pass
+    
+    @abstractmethod
+    def perimeter(self):
+        """An abstract method perimeter
+        """
+        pass
+    
+class Circle(Shape):
+    """A class that define Circle
+    base on Shape
+    """
+    def __init__(self, radius=0):
+        """Instan"""
+        self.radius = radius
+        
+    def area(self):
+        return math.pi * self.radius ** 2
+    
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+    
+class Rectangle(Shape):
+    """A class Rectangle base on
+    shape
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+def shape_info(shape):
+    """A func to print are and perimeter
+    """
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")   
