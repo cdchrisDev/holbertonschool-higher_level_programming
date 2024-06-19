e2eac04fd3ea0015cee8d7e9d071dabfd038497f
cdchrisDev <juan.feli.lopez@gmail.com>
2024-06-19 16:05:06 +0000

    1st try [update]

--- restful-api/task_02_request.py
+++ restful-api/task_02_request.py
@@ -2,18 +2,18 @@
 """This is the task_02_request
 Module
 """
-import requests, json
-
-
+import requests
+import json
+import csv
 url = 'https://jsonplaceholder.typicode.com/posts'
 r = requests.get(url)
 urlSuccess = True if r.status_code == 200 else False
+
 
 def fetch_and_print_posts():
     """A func that fetches post from
     JSONPlaceholder
     """
-    print(f"Status Code: {r.status_code}")
     try:
         r.raise_for_status()
     except request.RequestException as e:
@@ -24,13 +24,31 @@ def fetch_and_print_posts():
         json_data = r.json()
         for post in json_data:
             print(post["title"])
-    
-"""         
+
+
 def fetch_and_save_posts():
-    A func that fetches all post from
+    """A func that fetches all post from
     JSONPlaceholder
     """
-     
+    try:
+        json_data = r.json()
+    except ValueError:
+        print("Failed to get data")
+        return
+    finally:
+        csvF = "posts.csv"
+        filtered_data = [{key: post[key]
+                         for key in ('id',
+                         'title', 'body')}
+                         for post in json_data]
+        headers = ['id', 'title', 'body']
+
+    with open(csvF, 'w', newline="") as F:
+        csv_write = csv.DictWriter(F, fieldnames=headers)
+        csv_write.writeheader()
+        csv_write.writerows(filtered_data)
+
+
 if __name__ == "__main__":
     fetch_and_print_posts()
-    #fetch_and_save_posts()
+    fetch_and_save_posts()
