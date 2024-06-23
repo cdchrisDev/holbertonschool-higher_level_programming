8cc4ecdd546d7caf5846636e04334edaab23d9ad
cdchrisDev <juan.feli.lopez@gmail.com>
2024-06-23 23:12:40 +0000

    2nd try

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,15 +1,9 @@
 -- Create a second table with specific records
-CREATE TABLE IF NOT EXISTS second_table
-(id INT, name VARCHAR(256), score INT);
--- Query for Jhon
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT NOT NULL,
+	name VARCHAR(256),
+	score INT
+);
+-- Insert data respectively
 INSERT INTO second_table (id, name, score)
- VALUES (1, 'John', 10);
--- Query for Alex
-INSERT INTO second_table (id, name, score)
- VALUES (2, 'Alex', 3);
--- Query for Bob
-INSERT INTO second_table (id, name, score)
- VALUES (3, 'Bob', 14):
--- Query for George
-INSERT INTO second_table (id, name, score)
- VALUES (4, 'George', 8);
+VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
