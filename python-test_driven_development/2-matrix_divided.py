9acc7af38035a7caa3e7c436b38fd7bde68fc568
cdchrisDev <juan.feli.lopez@gmail.com>
2024-06-25 17:37:20 -0500

    hol

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,26 +1,27 @@
 #!/usr/bin/python3
-"""
-Divides a matrix
-"""
+'''function divided elements of the matrix'''
 
 
 def matrix_divided(matrix, div):
-    """
-    Function that divides a matrix
-    """
-
-    if type(matrix) != list:
-        raise TypeError("matrix must be a matrix\
-                        (list of list) of integers/floats")
-    for item in range(len(matrix)):
-        if item != 0:
-            result = item - 1
-            if len(matrix[item]) != len(matrix[result]):
-                raise TypeError("Each row of the matrix\
-                                must have the same size")
-    if isinstance(div, int) is False:
-        raise TypeError('div must be a number')
-    if div == 0:
-        raise ZeroDivisionError('division by zero')
-
-    return [[round(item / div, 2) for item in m_list] for m_list in matrix]
+    '''variable auxiliar'''
+    aux = len(matrix[0])
+    '''matrix is int or float'''
+    if not all(list(map(lambda x: all(list(map(lambda num:
+               isinstance(num, (int, float)), x))), matrix))):
+        '''error of the matrix'''
+        raise TypeError("matrix must be a matrix (list of lists) "
+                        "of integers/floats")
+    elif not all(list(map(lambda x: len(x) == aux, matrix))):
+        '''not it same size'''
+        raise TypeError("Each row of the matrix must have the same size")
+    elif not isinstance(div, (int, float)):
+        '''div not is int or float'''
+        raise TypeError("div must be a number")
+    elif div == 0:
+        '''div is zero'''
+        raise ZeroDivisionError("division by zero")
+    '''operation matrix'''
+    result = list(map(lambda x: list(map(lambda y:
+                  round(y / div, 2), x)), matrix))
+    '''return the result'''
+    return result
