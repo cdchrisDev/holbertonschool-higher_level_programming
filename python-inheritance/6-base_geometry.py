65802572083466405d859300efbad7f6fc58a307
cdchrisDev <juan.feli.lopez@gmail.com>
2024-08-03 19:58:07 -0500

    2nd try

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -5,6 +5,8 @@ Write a class BaseGeometry (based on 5-base_geometry.py)
 
 
 class BaseGeometry:
+    """A base geomery class"""
 
     def area(self):
+        """A func that gives area of"""
         raise Exception('area() is not implemented')
